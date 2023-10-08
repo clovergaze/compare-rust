a81cc09ba7f643c37f5fe9c7c598b2063821b722
Johannes Hillert <johannes.hillert@infokin.org>
2023-10-08 12:53:08 +0200

    Refactor repetitive parts into reusable functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{Read, Result};
 use std::process;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
   let args: Vec<String> = env::args().collect();
   if args.len() < 3 {
     println!("Syntax: compare <file1> <file2>");
@@ -11,47 +11,42 @@ fn main() -> io::Result<()> {
   }
 
   let filename1 = &args[1];
-  let mut file1 = File::open(filename1).unwrap_or_else(|_| {
-    println!("Error opening file \"{}\".", filename1);
-    process::exit(1);
-  });
-
-  let filesize1 = file1.metadata().unwrap().len();
+  let (mut file1, filesize1) = open_file(filename1)?;
 
   let filename2 = &args[2];
-  let mut file2 = File::open(filename2).unwrap_or_else(|_| {
-    println!("Error opening file \"{}\".", filename2);
-    process::exit(1);
-  });
-
-  let filesize2 = file2.metadata().unwrap().len();
+  let (mut file2, filesize2) = open_file(filename2)?;
 
   if filesize1 != filesize2 {
     println!("Files are not identical, the size is unequal.");
     process::exit(0);
   }
 
-  let mut buffer1 = vec![0u8; filesize1 as usize];
+  let buffer1 = read_file(&mut file1, filesize1)?;
+  let buffer2 = read_file(&mut file2, filesize2)?;
 
-  file1.read_exact(&mut buffer1).unwrap_or_else(|_| {
-    println!("Error reading file \"{}\".", filename1);
-    process::exit(1);
-  });
-
-  let mut buffer2 = vec![0u8; filesize2 as usize];
-
-  file2.read_exact(&mut buffer2).unwrap_or_else(|_| {
-    println!("Error reading file \"{}\".", filename2);
-    process::exit(1);
-  });
-
-  for i in 0..filesize1 {
-    if buffer1[i as usize] != buffer2[i as usize] {
-      println!("Files are not identical.");
-      process::exit(0);
-    }
+  if buffer1 != buffer2 {
+    println!("Files are not identical.");
+    process::exit(0);
   }
 
   println!("Files are identical.");
   Ok(())
+}
+
+fn open_file(filename: &str) -> Result<(File, u64)> {
+  let file = File::open(filename).unwrap_or_else(|_| {
+    println!("Error opening file \"{}\".", filename);
+    process::exit(1);
+  });
+  let filesize = file.metadata()?.len();
+  Ok((file, filesize))
+}
+
+fn read_file(file: &mut File, filesize: u64) -> Result<Vec<u8>> {
+  let mut buffer = vec![0u8; filesize as usize];
+  file.read_exact(&mut buffer).unwrap_or_else(|_| {
+    println!("Error reading file.");
+    process::exit(1);
+  });
+  Ok(buffer)
 }
